9173a4f1128ed55ef34571a4316243ad7551289f
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2022-09-03 20:43:53 +0100

    Update D.java

--- Round #817- Div 4/D.java
+++ Round #817- Div 4/D.java
@@ -4,15 +4,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class D {
-
+ 
 	public static void main(String[] args) {
 		
 		FastScanner sc = new FastScanner();
 		//number of test cases
 		int n = sc.nextInt();
-
+ 
 		//LRRLL
 		//k   j
 		//second input
@@ -28,31 +28,42 @@ public class D {
 			
 			
 			
-			while(k <= j && changes > 1) {
-				if(array[k] == 'R') {
-					k++;
+			while(k <= j && changes >= 1) {
+				if(mid-k > j - mid) {
+					if(array[k] == 'R') {
+						k++;
+					}
+					else {
+						array[k] = 'R';
+						
+						result -= k;
+						result += str.length()-k-1;
+						//result = countSum(new String(array));
+						changes--;
+						k++;
+						
+						System.out.print(result +" ");
+					}
 				}
-				else if(mid-k >= j - mid) {
-					array[k] = 'R';
-					result += str.length() - k -1;
-					//result = countSum(new String(array));
-					changes--;
-					k++;
-					
-					System.out.print(result +" ");
+				else {
+					if(array[j]== 'L') {
+						j--;
+					}
+					else{
+						array[j] = 'L';
+						//result = countSum(new String(array));
+						//take away prev value
+						result -=  (str.length()-j -1);
+						result += j;
+						//result += j - (str.length()-j);
+						changes--;
+						j--;
+						
+						System.out.print(result+" ");
+					}	
 				}
-				else if(array[j]== 'L') {
-					j--;
-				}
-				else if(j-mid >= mid-k) {
-					array[j] = 'L';
-					//result = countSum(new String(array));
-					result += j-1;
-					changes--;
-					j--;
-					
-					System.out.print(result+" ");
-				}
+			
+				
 					
 				
 				
@@ -68,7 +79,7 @@ public class D {
 			System.out.println();
 		}
 		
-
+ 
 	}
 	
 	public static long countSum(String str) {
@@ -82,11 +93,11 @@ public class D {
 		
 		return result;
 	}
-
+ 
 	public static class FastScanner {
 		BufferedReader br;
 		StringTokenizer st;
-
+ 
 		public FastScanner(String s) {
 			try {
 				br = new BufferedReader(new FileReader(s));
@@ -95,11 +106,11 @@ public class D {
 				e.printStackTrace();
 			}
 		}
-
+ 
 		public FastScanner() {
 			br = new BufferedReader(new InputStreamReader(System.in));
 		}
-
+ 
 		String nextToken() {
 			while (st == null || !st.hasMoreElements()) {
 				try {
@@ -111,15 +122,15 @@ public class D {
 			}
 			return st.nextToken();
 		}
-
+ 
 		int nextInt() {
 			return Integer.parseInt(nextToken());
 		}
-
+ 
 		long nextLong() {
 			return Long.parseLong(nextToken());
 		}
-
+ 
 		double nextDouble() {
 			return Double.parseDouble(nextToken());
 		}
